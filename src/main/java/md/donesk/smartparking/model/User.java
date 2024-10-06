1b43da58df407c0e3be3a320ef2d4ceeaa274d7b
Chiril Dones <doneskirill3@gmail.com>
2024-10-06 20:03:54 +0300

    add model ParkingSlot, Reservation, User

---
+++ src/main/java/md/donesk/smartparking/model/User.java
@@ -0,0 +1,29 @@
+package md.donesk.smartparking.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+
+    @CreatedDate
+    private Date createdAt;
+
+    @LastModifiedDate
+    private Date updatedAt;
+
+}
