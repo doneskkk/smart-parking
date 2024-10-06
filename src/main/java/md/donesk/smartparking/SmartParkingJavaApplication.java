6e4f61d7f2a7260d81d8a609050bd59e4c48f160
Chiril Dones <doneskirill3@gmail.com>
2024-10-06 17:36:38 +0300

    init commit

---
+++ src/main/java/md/donesk/smartparking/SmartParkingJavaApplication.java
@@ -0,0 +1,13 @@
+package md.donesk.smartparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartParkingJavaApplication.class, args);
+    }
+
+}
